af619529838ade23329e6a1f5035ecf4a3bfc373
Rahim2725 <info.abdurrahim12@gmail.com>
2022-04-08 15:45:03 +0600

    nput button add

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA9zdDszQGF0Y6M-p7l6cRbez5rMwY9rro",
+  authDomain: "email-password-auth-46b39.firebaseapp.com",
+  projectId: "email-password-auth-46b39",
+  storageBucket: "email-password-auth-46b39.appspot.com",
+  messagingSenderId: "124589430721",
+  appId: "1:124589430721:web:871e265de24e7718ae0140"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app ;
